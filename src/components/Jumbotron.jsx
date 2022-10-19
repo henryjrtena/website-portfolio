22fc4e724673bb8f7a9fb7d0c94f90022af5afe8
Henry Tena <106600653+henryjrtena@users.noreply.github.com>
2022-10-19 17:13:16 +0800

    fixed link

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -7,8 +7,8 @@ export default function Jumbotron(){
             <h1 className='name'>Henry Tena</h1>
             <h2>I'm a Full-Stack Web Developer.</h2>
             <div className='jumbotron--buttons'>
-                <button href='#' className='jumbo--projects'>Check Out My Projects</button>
-                <button href='#' className='jumbo--resume'>View My Resume</button>
+                <button href='#projects' className='jumbo--projects'>Check Out My Projects</button>
+                <button href='https://drive.google.com/drive/folders/1j9wLXRAUr3c5fGvA9lQ09xrhyiS1E3hu?usp=sharing' target="_blank" className='jumbo--resume'>View My Resume</button>
             </div>
         </div>
     )
