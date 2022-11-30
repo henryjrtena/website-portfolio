ad67d0e8601404fd5b6e572939eb8fe119ff6204
Henry Tena <106600653+henryjrtena@users.noreply.github.com>
2022-11-30 15:40:57 +0800

    add be proj

--- src/components/Data/BE_Proj.js
+++ src/components/Data/BE_Proj.js
@@ -13,7 +13,7 @@ export default {
             {
                 "id": 2,
                 "name": "Tenzies",
-                "url": "https://quizzard-nine.vercel.app/",
+                "url": "https://tenzies-zr0kr9xx8-henryjrtena.vercel.app/",
                 "madeWith": ['HTML', 'CSS', 'JavaScript', 'ReactJS'],
                 "imgDir": "project-images/tenzies.png",
                 "info": "Simple Tenzies game with JavaScript with Timer"
@@ -25,6 +25,15 @@ export default {
                 "madeWith": ['HTML', 'CSS', 'JavaScript', 'ReactJS'],
                 "imgDir": "project-images/quizzard.png",
                 "info": "Trivia question from OTDB API. User's can select difficulty and category."
+            },
+            ,
+            {
+                "id": 3,
+                "name": "Point-Set",
+                "url": "https://gilded-gingersnap-c4d5de.netlify.app/",
+                "madeWith": ['HTML', 'CSS', 'JavaScript', 'ReactJS', 'styled-components'],
+                "imgDir": "project-images/quizzard.png",
+                "info": "Users and click anywhere inside the window and will automatically put a circle dot. Can change color of the dot and background"
             }
         ]
     }
