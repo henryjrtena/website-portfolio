ddcb4d603bb413831301ca08d1339a81c771e57e
Henry Tena <106600653+henryjrtena@users.noreply.github.com>
2024-05-18 21:41:29 +0800

    added mvc as web app offer

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -12,6 +12,7 @@ export default function Jumbotron({ goto__projects }) {
             <h4>Hi. My name is</h4>
             <h1 className='name'>Henry Tena</h1>
             <h2>Mobile Developer with Flutter.</h2>
+            <h2>Web App Developer with ASP.NET MVC.</h2>
             <div className='jumbotron--buttons'>
                 <a target='_blank' href="https://drive.google.com/file/d/1xyYa42G9okso2DGnvLx5ukjtvbOMO5Gz/view?usp=sharing" className='jumbo--resume btnn'>View My Resume</a>
             </div>
