094077cfa4e54d7b8ab2fafd147069da55f1cccc
Henry Tena <106600653+henryjrtena@users.noreply.github.com>
2024-05-18 21:32:41 +0800

    added net as a skills

--- src/components/Data/Skills.js
+++ src/components/Data/Skills.js
@@ -14,26 +14,31 @@ export default {
             },
             {
                 "id": 3,
+                "name": ".NET",
+                "url": "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/dot-net.svg"
+            },
+            {
+                "id": 4,
                 "name": "Javascript",
                 "url": "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg"
             },
             {
-                "id": 4,
+                "id": 5,
                 "name": "ReactJS",
                 "url": "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg"
             },
             {
-                "id": 5,
+                "id": 6,
                 "name": "HTML5",
                 "url": "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg"
             },
             {
-                "id": 6,
+                "id": 7,
                 "name": "CSS",
                 "url": "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg"
             },
             {
-                "id": 7,
+                "id": 8,
                 "name": "SASS",
                 "url": "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/sass-colored.svg"
             }
